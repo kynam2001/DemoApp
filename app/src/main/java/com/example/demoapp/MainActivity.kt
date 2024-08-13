0665a549f55643a67d050d28cd678f5691b2efca
kynam <kynamnguyen2001@gmail.com>
2024-08-13 15:10:39 +0700

    commit 1

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var myInteger = 10;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.e("Log1","")
+        myInteger = 20
+        Log.e("Log2","")
+    }
+}
